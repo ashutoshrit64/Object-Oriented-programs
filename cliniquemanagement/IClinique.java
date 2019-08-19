f099dabd1e5560fe44ae495591880c65c643a3b2
Ashutosh <ashutoshrit64@gmail.com>
2019-08-19 19:34:01 +0530

    clinique

---
+++ cliniquemanagement/IClinique.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.oops.cliniquemanagement;
+
+public interface IClinique {
+	 String name="";
+	 int id=0;
+	 String specialization="";
+	 String avl="";
+	 String mobNumber="";
+	 int age=0;
+	String doctorFile = "/home/user/address/jsonfiles/CliniqueManagement/doctor.json";
+	String PatientFiles = "/home/user/address/jsonfiles/CliniqueManagement/patient.json";
+	String appointmentFile = "/home/user/address/jsonfiles/CliniqueManagement/appointment.json";
+
+}
